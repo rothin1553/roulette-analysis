d2ac70f65d0f7085a2f5283a6e69e76d731f25c7
Roth In <rothin1553@gmail.com>
2024-06-16 18:14:15 -0400

    fix one off rotation + add spin value

--- docs/roulette-visual/script.js
+++ docs/roulette-visual/script.js
@@ -54,7 +54,7 @@ function run_scenario1(test=5)
 
 
 let numRed = [1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36];
-let wheelnumbersAC = [0, 26, 3, 35, 12, 28, 7, 29, 18, 22, 9, 31, 14, 20, 1, 33, 16, 24, 5, 37 , 10, 23, 8, 30, 11, 36, 13, 27, 6, 34, 17, 25, 2, 21, 4, 19, 15, 32];
+let wheelnumbersAC = [0, 26, 3, 35, 12, 28, 7, 29, 18, 22, 9, 31, 14, 20, 1, 33, 16, 24, 5, 10, 23, 8, 30, 11, 36, 13, 27, 6, 34, 17, 25, 2, 21, 4, 19, 15, 32];
 
 let container = document.createElement('div');
 container.setAttribute('id', 'container');
@@ -544,8 +544,12 @@ function setBet(e, n, t, o){
 	}
 }
 
-function spin(){
+function spin(v=-1){
 	var winningSpin = Math.floor(Math.random() * 36);
+	if(v >= 0 && v <= 36)
+	{
+		winningSpin = v;
+	}
 	spinWheel(winningSpin);
 	setTimeout(function(){
 		if(numbersBet.includes(winningSpin)){
